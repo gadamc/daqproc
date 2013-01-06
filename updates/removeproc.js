1128ca55aefcf7b0fc6111cb04262e54930c2239
cox <adam.cox@kit.edu>
2013-01-06 14:35:33 -0800

    adding function to remove a procX. If the query passes in 'old_status' only docs with doc['status'] == old_status will be affected. If the query passes in 'new_status', the doc['status'] will be changed to 'new_status'. If the procX doesn't exist in the doc, it will still set doc['status'] to 'new_status'.

---
+++ updates/removeproc.js
@@ -0,0 +1,72 @@
+function(doc, req) {
+
+  var procname = req.query.procname;
+  var old_status = req.query.old_status;
+  var new_status = req.query.new_status;
+
+  var message = {};
+  if(req.query._showrequest == 'true')
+    message['request'] = req;
+
+  message['results'] = {};
+
+  var updateDoc = false;
+  var value;
+
+
+  if(procname == undefined){
+    message['results']['error'] = 'req.query.procname undefined';
+    return[null, JSON.stringify(message)]
+  }
+
+  if(procname.search('proc[0-9]$') != 0){
+    message['results']['error'] = 'req.query.procname not equal to proc[0-9]$';
+    return[null, JSON.stringify(message)]
+  }
+
+  function delFromDoc(aDoc, aprocName, theMessage){
+    if(aDoc[aprocName]){
+      theMessage['results']['success'] = 'removed ' + aprocName;
+      delete aDoc[aprocName];
+      return true;
+    } 
+    else{
+      theMessage['results']['success'] = 'document did not contain ' + aprocName;
+      return false;
+    }
+  }
+
+  function updateStatus(aDoc, aNewStatus, theMessage){
+    if(aNewStatus != undefined){
+      aDoc['status'] = aNewStatus;
+      theMessage['results']['new_status'] = aDoc['status'];
+      return true;
+    }
+    else return false;
+  }
+
+  if(old_status != undefined){
+    if(doc['status'] == old_status){
+      message['results']['status_condition'] = 'matched';
+      message['results']['old_status'] = old_status;
+      updateDoc = delFromDoc(doc, procname, message)
+      updateDoc = updateStatus(doc, new_status, message) || updateDoc;
+    }
+    else{
+      message['results']['status_condition'] = 'unmatched';
+      //do nothing when the doc['status'] != old_status.
+    }
+  }
+  else{
+    updateDoc = delFromDoc(doc, procname, message)
+    updateDoc = updateStatus(doc, new_status, message) || updateDoc;
+  }
+  
+
+  if(updateDoc)
+    return [doc, JSON.stringify(message)];
+
+  else
+    return [null, JSON.stringify(message)];
+
+}

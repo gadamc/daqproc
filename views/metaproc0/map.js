bd30786f5acb64d26e9b410e3098ec97572cf31d
gadamc <adam.cox@kit.edu>
2013-02-12 10:27:00 -0800

    removing stupid extra quote

--- views/metaproc0/map.js
+++ views/metaproc0/map.js
@@ -1,6 +1,6 @@
 function(doc) {
 
-    if(doc.type == "'daqmetadatadocuments'" && doc.status == "closed" && !doc.metaproc0){
+    if(doc.type == "daqmetadatadocuments" && doc.status == "closed" && !doc.metaproc0){
       emit( doc._id, doc.file_size);
     }
 }
